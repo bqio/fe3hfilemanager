df3ed5ea93d8ce2118eaaf05ad3c4c7236f9bc23
bqio <45086516+bqio@users.noreply.github.com>
2019-09-29 07:26:07 +0300

    Add files via upload

---
+++ FE3H File Manager/Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace FE3H_File_Manager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menu = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.databaseToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.checkUpdatesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.githubToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menu.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menu
+            // 
+            this.menu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.databaseToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menu.Location = new System.Drawing.Point(0, 0);
+            this.menu.Name = "menu";
+            this.menu.Size = new System.Drawing.Size(784, 24);
+            this.menu.TabIndex = 0;
+            this.menu.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.OpenToolStripMenuItem_Click);
+            // 
+            // databaseToolStripMenuItem
+            // 
+            this.databaseToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.checkUpdatesToolStripMenuItem});
+            this.databaseToolStripMenuItem.Name = "databaseToolStripMenuItem";
+            this.databaseToolStripMenuItem.Size = new System.Drawing.Size(67, 20);
+            this.databaseToolStripMenuItem.Text = "Database";
+            // 
+            // checkUpdatesToolStripMenuItem
+            // 
+            this.checkUpdatesToolStripMenuItem.Name = "checkUpdatesToolStripMenuItem";
+            this.checkUpdatesToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.checkUpdatesToolStripMenuItem.Text = "Update";
+            this.checkUpdatesToolStripMenuItem.Click += new System.EventHandler(this.CheckUpdatesToolStripMenuItem_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 539);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(784, 22);
+            this.statusStrip1.SizingGrip = false;
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(0, 17);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.dataGridView1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 24);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(784, 515);
+            this.panel1.TabIndex = 2;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ControlLightLight;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(784, 515);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.DataGridView1_CellDoubleClick);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.githubToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // githubToolStripMenuItem
+            // 
+            this.githubToolStripMenuItem.Name = "githubToolStripMenuItem";
+            this.githubToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.githubToolStripMenuItem.Text = "Github";
+            this.githubToolStripMenuItem.Click += new System.EventHandler(this.GithubToolStripMenuItem_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menu);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menu;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FE3H File Manager";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menu.ResumeLayout(false);
+            this.menu.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menu;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem databaseToolStripMenuItem;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripMenuItem checkUpdatesToolStripMenuItem;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem githubToolStripMenuItem;
+    }
+}
+
---
+++ FE3H File Manager/Form1.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ExcelDataReader;
+
+namespace FE3H_File_Manager
+{
+    public partial class Form1 : Form
+    {
+        List<string> database = new List<string>();
+        string dataPath = "";
+        string tmpPath = "";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void CheckUpdatesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            UpdateDataBase();
+        }
+
+        private void UpdateDataBase()
+        {
+            string url = "https://docs.google.com/spreadsheets/d/18bCCrsHwyAU-JSlpvaulVos3j8dtPBr0mDB-vLWib54/export?format=xlsx&id=18bCCrsHwyAU-JSlpvaulVos3j8dtPBr0mDB-vLWib54";
+            WebClient myWebClient = new WebClient();
+            myWebClient.DownloadFile(url, "db.xlsx");
+
+            DateTime dt = File.GetLastWriteTime("db.xlsx");
+            toolStripStatusLabel1.Text = "Last update: " + dt;
+
+            MessageBox.Show("Database successfully updated.", "Notification", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            LoadDataBase();
+        }
+
+        private void LoadDataBase()
+        {
+            using(var stream = File.Open("db.xlsx", FileMode.Open, FileAccess.Read))
+            {
+                using(var reader = ExcelReaderFactory.CreateReader(stream))
+                {
+                    var result = reader.AsDataSet();
+                    DataTable table = new DataTable();
+
+                    table.Columns.Add("Index");
+                    table.Columns.Add("Path");
+                    table.Columns.Add("Description");
+
+                    for (int i = 1; i < result.Tables[1].Rows.Count; i++)
+                    {
+                        DataRow dr = table.NewRow();
+
+                        dr["Index"] = result.Tables[1].Rows[i].ItemArray[0];
+                        dr["Path"] = result.Tables[1].Rows[i].ItemArray[2];
+                        dr["Description"] = result.Tables[1].Rows[i].ItemArray[3];
+
+                        table.Rows.Add(dr);
+                    }
+                    
+                    dataGridView1.DataSource = table;
+                    dataGridView1.Columns[1].Width = 600;
+
+                    dataGridView1.Update();
+                    WindowState = FormWindowState.Maximized;
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (File.Exists("db.xlsx"))
+            {
+                DateTime dt = File.GetLastWriteTime("db.xlsx");
+                toolStripStatusLabel1.Text = "Last update: " + dt;
+
+                LoadDataBase();
+            } else
+            {
+                UpdateDataBase();
+            }
+        }
+
+        private void TestToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            LoadDataBase();
+        }
+
+        private void OpenToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "DATA0.bin (*.bin)|*.bin";
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    dataPath = openFileDialog1.FileName;
+                    tmpPath = folderBrowserDialog1.SelectedPath;
+                }
+                else
+                {
+                    MessageBox.Show("You must specify a dist folder for the files.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("You must specify the file DATA0.bin.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void DataGridView1_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (dataPath.Length == 0 || tmpPath.Length == 0)
+            {
+                MessageBox.Show("Select the DATA0.bin file and the dist directory for the files in the open menu.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            int index = dataGridView1.CurrentCell.RowIndex;
+            int offset = index * 32;
+            string path = dataGridView1.Rows[index].Cells[1].Value.ToString();
+
+            if (path.Length == 0)
+            {
+                MessageBox.Show("File path not specified.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+
+                return;
+            }
+
+            using (var metaReader = new BinaryReader(File.OpenRead(dataPath)))
+            {
+                metaReader.BaseStream.Position = offset;
+
+                long foffset = metaReader.ReadInt64();
+                long funcompressedSize = metaReader.ReadInt64();
+                long fcompressedSize = metaReader.ReadInt64();
+                long fisCompressed = metaReader.ReadInt64();
+
+                string dataDir = Path.GetDirectoryName(dataPath);
+
+                using (var dataReader = new BinaryReader(File.OpenRead(dataDir + "\\DATA1.bin")))
+                {
+                    dataReader.BaseStream.Position = foffset;
+
+                    if (fisCompressed == 1)
+                    {
+                        if (fcompressedSize > 0)
+                        {
+                            if (!Directory.Exists(tmpPath + "\\" + Path.GetDirectoryName(path)))
+                            {
+                                Directory.CreateDirectory(tmpPath + "\\" + Path.GetDirectoryName(path));
+                            }
+
+                            File.WriteAllBytes(tmpPath + "\\" + path, dataReader.ReadBytes((int)funcompressedSize));
+
+                            MessageBox.Show("File structure successfully created.", "Notification", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        }
+                    } else
+                    {
+                        if (funcompressedSize > 0)
+                        {
+                            if (!Directory.Exists(tmpPath + "\\" + Path.GetDirectoryName(path)))
+                            {
+                                Directory.CreateDirectory(tmpPath + "\\" + Path.GetDirectoryName(path));
+                            }
+
+                            File.WriteAllBytes(tmpPath + "\\" + path, dataReader.ReadBytes((int)funcompressedSize));
+
+                            MessageBox.Show("File structure successfully created.", "Notification", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        }
+                    }
+                }
+            }
+        }
+
+        private void GithubToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://github.com/bqio/fe3hfilemanager");
+        }
+    }
+}
